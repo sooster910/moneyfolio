7e3f9c810897607b27af7115cdc6325e3ab5fc4e
hyunsu joo <hs9880@gmail.com>
2019-03-24 17:15:15 -0400

    add firebase configuration

---
+++ src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Replace this with your own config details
+var config = {
+    apiKey: "AIzaSyAySQ_TthJLOPznZt36KnksOjKQeH8cCHk",
+    authDomain: "expense-budget-app.firebaseapp.com",
+    databaseURL: "https://expense-budget-app.firebaseio.com",
+    projectId: "expense-budget-app",
+    storageBucket: "expense-budget-app.appspot.com",
+    messagingSenderId: "992485539146"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase 
