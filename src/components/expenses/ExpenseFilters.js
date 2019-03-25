170f5cda110b02887d3a2b39419d6774d09c37c9
hyunsu joo <hs9880@gmail.com>
2019-03-24 22:47:31 -0400

    Added ability to filter expense list

--- src/components/expenses/ExpenseFilters.js
+++ src/components/expenses/ExpenseFilters.js
@@ -1,14 +1,43 @@
 import React from 'react'
-
-const ExpenseFilters=(props)=>{
+import { connect } from 'react-redux';
+import { setTextFilter,setCategoryFilter } from '../../store/actions/filtersActions';
+const ExpenseFilters = (props) => {
     console.log(props);
 
-    return(<div>
-    
-      
+    return (
+        <div>
+        
+        <input type="text"
+                value={props.filters.text} 
+                onChange={(e) => {props.dispatch(setTextFilter(e.target.value))
+        }}/>
+
+        <select value={props.filters.category} 
+                onChange={(e)=>{props.dispatch(setCategoryFilter(e.target.value))}}
+            >    
+            <option value="select">select category</option>
+            <option value="education">Education and Training</option>
+            <option value="food">Food</option>
+            <option value="entertainment">Entertainment</option>
+            <option value="electronic">Electronic</option>
+            <option value="clothing">Clothing</option>
+            <option value="home">Home</option>
+            <option value="travel">Travel</option>
+            <option value="utilities">Utilities</option>
+            <option value="transportation">Transportation</option>
+            <option value="savings">Savings</option>
+            <option value="debt">Debt</option>
+            <option value="other">Other</option>
+
+        </select>
+
         </div>
     )
 
 }
-
-export default ExpenseFilters;
+const mapStateToProps = (state) => {
+    return {
+        filters: state.filters
+    }
+}
+export default connect(mapStateToProps)(ExpenseFilters)
--- src/components/filter/filterLogic.js
+++ src/components/filter/filterLogic.js
@@ -1,19 +1,12 @@
 //filter logic 
 
-const filterLogic = (expenses, { text, sortBy, startDate, endDate }) => {
-    return expenses.filter((expense) => {
-      //const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-      //const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
-      const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
-  return textMatch;
-      //return startDateMatch && endDateMatch && textMatch;
-    // }).sort((a, b) => {
-    //   if (sortBy === 'date') {
-    //     return a.createdAt < b.createdAt ? 1 : -1;
-    //   } else if (sortBy === 'amount') {
-    //     return a.amount < b.amount ? 1 : -1;
-    //   }
-    // });
-})};
-
-  export default filterLogic;
+const filteredExpense=(expenses,{text, sortBy, startDate, endDate,category,amount})=>{
+    return expenses.filter((filteringExpense)=>{
+          const startDateMatch = typeof startDate!== 'number' || filteringExpense.createdAt>=startDate;
+          const endDateMatch =typeof endDate!=='number' || filteringExpense.createdAt<=endDate;
+          const textMatch= filteringExpense.description.toLowerCase().includes(text.toLowerCase());
+        
+          return startDateMatch && endDateMatch && textMatch;
+        })
+    }
+  export default filteredExpense;
