047d7bb401ab20a94d9193bced832265900028ac
hyunsu joo <hs9880@gmail.com>
2019-04-01 15:04:25 -0400

    refactor to combine ExpenseForm

--- src/components/expenses/AddExpense.js
+++ src/components/expenses/AddExpense.js
@@ -2,126 +2,19 @@ import React, { Component } from 'react';
 import 'react-dates/initialize';
 import { connect } from 'react-redux'
 import { addExpense} from '../../store/actions/expensesActions'
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import moment from 'moment';
-
+import ExpenseForm from './ExpenseForm'
 
 class AddExpense extends Component {
 
-
-    state = {
-        description: '',
-        category: null,
-        note: '',
-        amount: 0,
-        createdAt: moment(this.props.createdAt).valueOf(),
-        calendarFocused: false,
-        Message: ''
-    };
-
-    handleDescription = (e) => {
-        const description = e.target.value;
-        this.setState(() => ({
-            description: description
-        }))
-    }
-    handleCategory = (e) => {
-        const category = e.target.value;
-        this.setState(() => ({
-            category: category
-        }))
-    }
-    handleTextarea = (e) => {
-        const note = e.target.value;
-        this.setState(() => ({
-            note: note
-        }))
-    }
-    handleAmount = (e) => {
-        const amount = e.target.value;
-        if (amount.match(/^\d*(\.\d{0,2})?$/)) {
-            this.setState(() => ({
-                amount: amount
-            }));
-        }
-    };
-    handleChange = (createdAt) => {
-        this.setState(() => ({
-            createdAt: createdAt
-        }));
-    }
-
-    handleFocusChange = ({ focused }) => {
-        this.setState(() => ({ calendarFocused: focused }));
-    };
-
-    onSubmit = (e) => {
-        e.preventDefault();
-        console.log('submit the from:', this.state)
-        if (!this.state.description || !this.state.amount || this.state.category === 'select') {
-            this.setState(() => ({ Message: 'Please provide description, amount and category' }));
-        } else {
-            this.setState(() => ({ Message: '' }));
-            this.props.addExpense(this.state);
-            this.props.history.push('/');
-        }
-
-    };
     render() {
         return (
             <div>
-                ExpenseForm
-
-                {this.state.Message && <p>{this.state.Message}</p>}
-                <form onSubmit={this.onSubmit}>
-                    <input type="text"
-                        placeholder="Description"
-                        autoFocus
-                        value={this.state.description}
-                        onChange={this.handleDescription} >
-
-                    </input>
-
-                    <select
-                        value={this.state.category}
-                        onChange={this.handleCategory}
-                    >
-                        <option value="select">select category</option>
-                        <option value="education">Education and Training</option>
-                        <option value="food">Food</option>
-                        <option value="entertainment">Entertainment</option>
-                        <option value="electronic">Electronic</option>
-                        <option value="clothing">Clothing</option>
-                        <option value="home">Home</option>
-                        <option value="travel">Travel</option>
-                        <option value="utilities">Utilities</option>
-                        <option value="transportation">Transportation</option>
-                        <option value="savings">Savings</option>
-                        <option value="debt">Debt</option>
-                        <option value="other">Other</option>
-
-                    </select>
-
-                    <input type="number"
-                        placeholder="Amount"
-                        value={this.state.amount}
-                        onChange={this.handleAmount}
-                    />
-
-                    <DatePicker
-                        selected={this.state.createdAt}
-                        onSelect={this.handleCreatedAt} //when day is clicked
-                        onChange={this.handleChange} //only when value has changed
-                    />
-
-                    <textarea placeholder="Add a optional note for your expense"
-                        value={this.state.note}
-                        onChange={this.handleTextarea}>
-                    </textarea>
-
-                    <button>Add Expense</button>
-                </form>
+               <ExpenseForm onSubmit={(expense)=>{
+                   this.props.addExpense(expense)
+                    this.props.history.push('/');
+             
+                   
+               }} />               
             </div>
         )
     }
@@ -129,7 +22,6 @@ class AddExpense extends Component {
 const mapDispatchToProps = dispatch => {
     return {
         addExpense: (expense) => dispatch(addExpense(expense)),
-
     }
 }
 export default connect(null, mapDispatchToProps)(AddExpense)
--- src/components/expenses/EditExpense.js
+++ src/components/expenses/EditExpense.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { editExpense, removeExpense } from '../../store/actions/expensesActions';
+import ExpenseForm from './ExpenseForm';
 
-const EditExpense=(props)=>{
-    console.log(props);
 
-    return(<div>
-        <h1>EditExpense</h1>
-        <h3>id: {props.match.params.id}</h3>
+class EditExpense extends Component {
+
+    onSubmit = (expense) => {
+        this.props.editExpense(this.props.matchingExpense.id, expense);
+        this.props.history.push('/');
+    }
+    render(){
+        return (
+        <div>
+            <h3>Editing the expense with id of {this.props.match.params.id}</h3>
+            <ExpenseForm
+                matchingExpense={this.props.matchingExpense}
+                onSubmit={this.onSubmit} />
         </div>
-    )
+        );
+    }
 
 }
-
-export default EditExpense;
+const mapStateToProps = (state, props) => {
+    return {
+        matchingExpense: state.expense.find((eachExpense) => {
+            console.log('mapstate:', eachExpense.id)
+            console.log('mapstate:', eachExpense)
+            return eachExpense.id === props.match.params.id
+        })
+    }
+}
+const mapDispatchToProps = (dispatch, props) => ({
+    editExpense: (id, expense) => dispatch(editExpense(id, expense))
+})
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpense);
