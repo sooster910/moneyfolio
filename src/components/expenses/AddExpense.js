c322eb8514eab1cfaeca57f712a3460f0984a90a
hyunsu joo <hs9880@gmail.com>
2019-04-04 22:21:36 -0400

    add redirect

--- src/components/expenses/AddExpense.js
+++ src/components/expenses/AddExpense.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import 'react-dates/initialize';
 import { connect } from 'react-redux'
-import { addExpense} from '../../store/actions/expensesActions'
+import { addExpense } from '../../store/actions/expensesActions'
 import ExpenseForm from './ExpenseForm'
-
+import { Redirect } from 'react-router-dom'
 class AddExpense extends Component {
-
     render() {
+        const { auth } = this.props;
+        if (!auth.uid) return <Redirect to='/' />
         return (
             <div>
-               <ExpenseForm onSubmit={(expense)=>{
-                   this.props.addExpense(expense)
-                    this.props.history.push('/');
-             
-                   
-               }} />               
+                <ExpenseForm onSubmit={(expense) => {
+                    this.props.addExpense(expense)
+                    this.props.history.push('/dashboard');
+                }} />
             </div>
         )
+    }
+}
+const mapStateToProps = (state) => {
+    console.log('auth state:', state)
+    return {
+        auth: state.auth
     }
 }
 const mapDispatchToProps = dispatch => {
@@ -24,4 +29,4 @@ const mapDispatchToProps = dispatch => {
         addExpense: (expense) => dispatch(addExpense(expense)),
     }
 }
-export default connect(null, mapDispatchToProps)(AddExpense)
+export default connect(mapStateToProps, mapDispatchToProps)(AddExpense)
--- src/components/expenses/EditExpense.js
+++ src/components/expenses/EditExpense.js
@@ -2,21 +2,31 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { editExpense, removeExpense } from '../../store/actions/expensesActions';
 import ExpenseForm from './ExpenseForm';
+import {Redirect} from 'react-router-dom'
 
 
 class EditExpense extends Component {
 
     onSubmit = (expense) => {
         this.props.editExpense(this.props.matchingExpense.id, expense);
-        this.props.history.push('/');
+        this.props.history.push('/dashboard');
     }
+    handleRemove=()=>{
+        this.props.removeExpense({ id: this.props.matchingExpense.id });
+        this.props.history.push('/dashboard');
+    }
+   
     render(){
+        const {auth} = this.props;
+        if(!auth.uid) return<Redirect to='/'/>
+       console.log('current auth id Editexpense:', this.props.auth)
         return (
         <div>
             <h3>Editing the expense with id of {this.props.match.params.id}</h3>
             <ExpenseForm
                 matchingExpense={this.props.matchingExpense}
                 onSubmit={this.onSubmit} />
+             <button onClick={this.handleRemove} className="btn btn-danger" >Remove</button> 
         </div>
         );
     }
@@ -24,14 +34,17 @@ class EditExpense extends Component {
 }
 const mapStateToProps = (state, props) => {
     return {
+        auth: state.auth,   
         matchingExpense: state.expense.find((eachExpense) => {
             console.log('mapstate:', eachExpense.id)
             console.log('mapstate:', eachExpense)
             return eachExpense.id === props.match.params.id
+      
         })
     }
 }
 const mapDispatchToProps = (dispatch, props) => ({
-    editExpense: (id, expense) => dispatch(editExpense(id, expense))
+    editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+    removeExpense: (expense)=>dispatch(removeExpense(expense))
 })
 export default connect(mapStateToProps, mapDispatchToProps)(EditExpense);
