14cf608a735ad5f457c0644a0a32efef758722f7
hyunsu joo <hs9880@gmail.com>
2019-04-04 22:18:03 -0400

    added abililty to see private route

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,18 +3,25 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedoutLinks';
+import firebase from '../../config/fbConfig'
+import { connect } from 'react-redux'
 
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const { auth } = props;
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
+
   return (
-   
-      <div>
-        <Link to='/'>Moneyfolio</Link> 
-        <SignedInLinks />
-        <SignedOutLinks/>
-      </div>
- 
+    <div>
+      <Link to='/dashboard'>Moneyfolio</Link>
+      {links}
+    </div>
   )
 }
-
-export default Navbar
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+    auth: state.firebase.auth
+  }
+}
+export default connect(mapStateToProps)(Navbar)
